6dd032f9d13185bc80dd8ef5198b30e2ea8f218e
Eric <ekameyshaaa@gmail.com>
2021-08-18 14:19:52 +0300

    add product header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,34 @@
 $(function(){
+  $('.product-slide__thumb').slick({
+     asNavFor: '.product-slide__big',
+      focusOnSelect: true,
+      slidesToShow: 4,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false
+  });
+  $('.product-slide__big').slick({
+     asNavFor: '.product-slide__thumb',
+     draggable: false,
+     arrows: false,
+     fade: true
+  });
+
+
+  $('.select-style, .product-one__num').styler()
+  $('.filter-price__input').ionRangeSlider({
+    type: 'double',
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from)
+      $('.filter-price__to').text(data.to)
+    },
+
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from)
+      $('.filter-price__to').text(data.to)
+    },
+  });
   $('.top-slider__inner').slick({
  dots: true,
  arrows: false,
